d8829801a09333432de6910470b207531059d69c
Luciene <lucienebsousa@gmail.com>
2026-05-02 20:24:05 -0300

    heuristicas

--- src/controllers/heuristicas.controller.js
+++ src/controllers/heuristicas.controller.js
@@ -1,3 +1,35 @@
+/* const heuristicasService = require("../services/heuristicas.service");
+
+const show = async (req, res, next) => {
+  try {
+    const heuristica = await heuristicasService.getHeuristicaById({
+      id: req.params.id,
+      usuarioId: req.user.id,
+    });
+
+    return res.status(200).json({ heuristica });
+    const create = async (req, res, next) => {
+      try {
+        const { nome, descricao } = req.body;
+
+        const heuristica = await heuristicasService.createHeuristica({
+          nome,
+          descricao,
+          usuarioId: req.user.id,
+        });
+
+        return res.status(201).json({ heuristica });
+      } catch (error) {
+        return next(error);
+      }
+    };
+
+    module.exports = {
+      show,
+      create,
+    };
+ */
+
 const heuristicasService = require("../services/heuristicas.service");
 
 const show = async (req, res, next) => {
@@ -8,6 +40,11 @@ const show = async (req, res, next) => {
     });
 
     return res.status(200).json({ heuristica });
+  } catch (error) {
+    return next(error);
+  }
+};
+
 const create = async (req, res, next) => {
   try {
     const { nome, descricao } = req.body;
